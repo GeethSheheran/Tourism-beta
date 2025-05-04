15f3028e40564bb29aa9fc346c175c0830189b49
geethsheheran <68048419+GeethSheheran@users.noreply.github.com>
2025-05-04 20:33:21 +0530

    jsom/commit

---
+++ ruizarch/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,20 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/layout.jsx": [
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/_styles/scss/style.scss",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/globals.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/_styles/css/plugins/bootstrap-grid.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/_styles/css/plugins/swiper.min.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/_styles/css/plugins/magnific-popup.css"
+    ],
+    "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/(pages)/page.jsx": [
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/node_modules/react-modal-video/css/modal-video.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/node_modules/swiper/swiper.min.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/node_modules/swiper/modules/navigation/navigation.min.css",
+      "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/node_modules/swiper/modules/pagination/pagination.min.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/admin/Documents/GitHub/Tourism-beta/ruizarch/src/app/(pages)/page": []
+  }
+}
